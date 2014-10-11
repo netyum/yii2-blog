3cbcddf60375cfa9be8e318a2a44f61ce8136130
netyum <netyum@163.com>
2014-10-11 13:16:38 +0800

    Update

--- config/web.php
+++ config/web.php
@@ -40,6 +40,10 @@ $config = [
         ]
     ],
     'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'yii2-blog',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
